865fd586c268df75cdf928843fe40a0a3fb9176f
Jerry Doran <Jerry.Doran@pccairfoils.com>
2020-06-26 12:32:30 -0400

    Added meta tag for description, added top scroll link button

--- js/main.js
+++ js/main.js
@@ -1,9 +1,20 @@
-$('.menu').on('click', function() {
+$('.menu').on('click', function () {
   $(this).toggleClass('active');
   $('.overlay').toggleClass('menu-open');
 });
 
-$('.nav a').on('click', function() {
+$('.nav a').on('click', function () {
   $('.menu').removeClass('active');
   $('.overlay').removeClass('menu-open');
+});
+
+const topLink = document.querySelector('.top-link');
+window.addEventListener('scroll', () => {
+  const scrollHeight = window.pageYOffset;
+
+  if (scrollHeight > 500) {
+    topLink.classList.add('show-link');
+  } else {
+    topLink.classList.remove('show-link');
+  }
 });
